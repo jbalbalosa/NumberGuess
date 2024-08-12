eab54ce3c0b20606e49642816d888d0031755ff2
Jeff Balbalosa <cyjeffb@yahoo.com>
2024-08-12 15:47:29 +0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int num1 = 0;
+int num2 = 0;
+
+Console.WriteLine("Guess a whole number");
+Console.WriteLine("Number 1:*");
+Console.Write("Enter a Number 2:");
+
+if (num1 == int.Parse(Console.ReadLine()))
+{
+    Console.WriteLine("Numbers are equal!");
+}
+else 
+{
+    Console.WriteLine("Numbers aren't equal");
+}
+
+Console.ReadLine();
